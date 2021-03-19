4a080088f7764adc60beab4789af7b0fb1bae640
serjkovalko <serjkovalko@gmail.com>
2021-03-19 14:34:10 +0200

    added product

--- gulpfile.js
+++ gulpfile.js
@@ -40,10 +40,11 @@ function images() {
 
 function scripts() {
    return src([
-      'node_modules/jquery/dist/jquery.js',
-      'node_modules/slick-carousel/slick/slick.js',
+     'node_modules/jquery/dist/jquery.js',
+     'node_modules/slick-carousel/slick/slick.js',
      'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-      'app/js/main.js'
+     'node_modules/rateyo/src/jquery.rateyo.js',
+     'app/js/main.js'
    ])
       .pipe(concat('main.min.js'))
       .pipe(uglify())
